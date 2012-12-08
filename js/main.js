766c543128dc121300ab86e006770695616d7b55
c9s <cornelius.howl@gmail.com>
2012-12-09 01:22:34 +0800

    wrap jquery with define.

--- js/main.js
+++ js/main.js
@@ -1,6 +1,14 @@
+require(["cs","coffee-script","jquery"], ['cs!csmain'] );
+
+/*
 require({
   paths: {
     cs: 'cs',
     'coffee-script': 'coffee-script'
+    "jquery":"jquery",
+    // "jquery.alpha":"jquery.alpha",
+    // "jquery.beta":"jquery.beta"
   }
 }, ['cs!csmain']);
+*/
+
