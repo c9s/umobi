3bedf7a2360b77d1fba1964569c547ef6539317c
c9s <cornelius.howl@gmail.com>
2012-12-11 13:03:08 +0800

    Add getTags method to dom

--- js/umobi.dom.js
+++ js/umobi.dom.js
@@ -1,3 +1,8 @@
+// query selector vs jquery
+// http://jsperf.com/jquery-vs-queryselectorall-to-array
+//
+// get element by id vs query selector
+// http://jsperf.com/getelementbyid-v-s-queryselector
 define(["jquery","cs!umobi.core"],function($) {
     var dom = {  };
     dom.queryAll = function(q) {
@@ -5,8 +10,15 @@ define(["jquery","cs!umobi.core"],function($) {
             return $(document.querySelectorAll(q));
         return $(q);
     };
+
+    // get by id
     dom.get = function(id) {
         return $(document.getElementById(id));
+    };
+
+    // get by tagname
+    dom.getTags = function(tag) {
+        return $(document.getElementsByTagName(tag));
     };
     uMobi.dom = dom;
     return dom;
