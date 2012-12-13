28c89980418305173786f09cf0e4b62d5562a1f8
c9s <cornelius.howl@gmail.com>
2012-12-13 13:52:37 +0800

    retab css.js file

--- build/tasks/css.js
+++ build/tasks/css.js
@@ -1,60 +1,59 @@
 var requirejs = require( 'requirejs' ),
-	path = require( 'path' ),
-	fs = require( 'fs' ),
-	util = require( 'util' );
+    path = require( 'path' ),
+    fs = require( 'fs' ),
+    util = require( 'util' );
 
 module.exports = function( grunt ) {
-	var config = grunt.config.get( 'global' ),
-		helpers = config.helpers;
+    var config = grunt.config.get( 'global' ),
+        helpers = config.helpers;
 
-	grunt.registerTask( 'css:compile', 'use require js to sort out deps', function() {
-		var theme = grunt.config.get( 'css' ).theme,
-			themeFile = grunt.config.get( 'css' ).themeFile,
-			require = grunt.config.get( 'css' ).require;
+    grunt.registerTask( 'css:compile', 'use require js to sort out deps', function() {
+        var theme = grunt.config.get( 'css' ).theme,
+            themeFile = grunt.config.get( 'css' ).themeFile,
+            require = grunt.config.get( 'css' ).require;
 
-		// pull the includes together using require js
-		requirejs.optimize( require.all );
+        // pull the includes together using require js
+        requirejs.optimize( require.all );
 
-		// pull the includes together using require js
-		requirejs.optimize( require.structure );
+        // pull the includes together using require js
+        requirejs.optimize( require.structure );
 
-		// simple theme file compile
-		grunt.file.write( themeFile + '.css', 'css/themes/' + theme + '/umobi.css' );
-	});
-
-    grunt.registerTask( 'css:images', 'copy images', function() {
-		var done = this.async(),
-			theme = grunt.config.get( 'css' ).theme,
-			require = grunt.config.get( 'css' ).require,
-			global_config = grunt.config.get( 'global' );
-
-		// copy images directory
-		var imagesPath = path.join( global_config.dirs.output, 'images' ), fileCount = 0;
-		grunt.file.mkdir( imagesPath );
-		grunt.file.recurse( 'css/themes/' + theme + '/images', function( full, root, sub, filename ) {
-			fileCount++;
-			var is = fs.createReadStream( full );
-			var os = fs.createWriteStream( path.join(imagesPath, filename) );
-			util.pump(is, os, function() {
-				fileCount--;
-				if( fileCount == 0 ) { done(); }
-			});
-		});
+        // simple theme file compile
+        grunt.file.write( themeFile + '.css', 'css/themes/' + theme + '/umobi.css' );
     });
 
-	// TODO image copy would be better in compile though not perfect
-	grunt.registerTask( 'css:cleanup', 'compile and minify the css', function() {
-		var theme = grunt.config.get( 'css' ).theme,
-			require = grunt.config.get( 'css' ).require,
-			global_config = grunt.config.get( 'global' );
+    // TODO image copy would be better in compile though not perfect
+    grunt.registerTask( 'css:images', 'copy images for css', function() {
+        var done          = this.async(),
+            theme         = grunt.config.get( 'css' ).theme,
+            require       = grunt.config.get( 'css' ).require,
+            global_config = grunt.config.get( 'global' );
 
-		// remove the requirejs compile output
-		fs.unlink( require.all.out );
-		fs.unlink( require.structure.out );
+        // copy images directory
+        var imagesPath = path.join( global_config.dirs.output, 'images' ), fileCount = 0;
+        grunt.file.mkdir( imagesPath );
+        grunt.file.recurse( 'css/themes/' + theme + '/images', function( full, root, sub, filename ) {
+            fileCount++;
+            var is = fs.createReadStream( full );
+            var os = fs.createWriteStream( path.join(imagesPath, filename) );
+            util.pump(is, os, function() {
+                fileCount--;
+                if( fileCount == 0 ) { done(); }
+            });
+        });
+    });
 
-	});
+    grunt.registerTask( 'css:cleanup', 'compile and minify the css', function() {
+        var theme = grunt.config.get( 'css' ).theme,
+            require = grunt.config.get( 'css' ).require,
+            global_config = grunt.config.get( 'global' );
 
-	// NOTE the progression of events is not obvious from the above
-	//      compile -> concat x 3 -> min all -> cleanup the compiled stuff
-	grunt.registerTask( 'css', 'config:async sass:compile css:compile concat:regular concat:structure concat:theme cssmin css:cleanup css:images' );
+        // remove the requirejs compile output
+        fs.unlink( require.all.out );
+        fs.unlink( require.structure.out );
+    });
+
+    // NOTE the progression of events is not obvious from the above
+    //      compile -> concat x 3 -> min all -> cleanup the compiled stuff
+    grunt.registerTask( 'css', 'config:async sass:compile css:compile concat:regular concat:structure concat:theme cssmin css:cleanup css:images' );
 };
