5ef03bc696cbe70c3ce4c4d5eefc0c7c5779955c
c9s <cornelius.howl@gmail.com>
2012-12-14 13:20:24 +0800

    Move external script to lib.

--- js/umobi.js
+++ js/umobi.js
@@ -3,15 +3,14 @@ require({
   // baseUrl: '/js',
   urlArgs: 'bust=' + (new Date()).getTime(),
   paths: {
-    "cs": 'cs',
-    "coffee-script": "coffee-script",
-    "jquery":"jquery"
+    "cs": "lib/cs",
+    "coffee-script": "lib/coffee-script",
+    "jquery":"lib/jquery"
    }
 });
 
 // load cs plugin and coffee-script
 document.getElementsByTagName('html')[0].className += 'ui-mobile ui-mobile-rendering';
-define(["jquery"], function() { 
-    require(["cs","coffee-script","cs!umobi.main"])
+define(["jquery","cs","coffee-script"], function(jQuery) { 
+    require(["cs!umobi.main"])
 });
-
