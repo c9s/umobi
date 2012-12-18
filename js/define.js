5e39496be55a45c1999efb2d4b93ffe1b8e11e9f
c9s <cornelius.howl@gmail.com>
2012-12-19 01:41:17 +0800

    Add js/define.js

---
+++ js/define.js
@@ -0,0 +1,7 @@
+if(typeof define === "undefined" ) {
+    // creates the define method on window, only used where async loading
+    // is not desired in the docs and experiments
+    window.define = function() {
+        Array.prototype.slice.call( arguments ).pop()( window.jQuery );
+    };
+}
