29a4652f5cf067749d126a60136f8e754d32cac8
c9s <cornelius.howl@gmail.com>
2013-01-09 16:56:55 +0800

    use _head.php

--- docs/index.php
+++ docs/index.php
@@ -1,16 +1,6 @@
 <!DOCTYPE html>
 <html>
-    <head>
-        <meta charset="utf-8">
-        <title>µMobi (uMobi)</title>
-        <meta name="apple-touch-fullscreen" content="yes" />
-        <meta name="apple-mobile-web-app-capable" content="yes" />
-        <meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no"/>
-        <link href="../css/structure/umobi.css" rel="stylesheet" type="text/css"/>
-        <link href="../css/themes/default/umobi.css" rel="stylesheet" type="text/css"/>
-        <link href="../css/customfont/fontawesome.css" rel="stylesheet" type="text/css"/>
-        <script src="../js"> </script>
-    </head>
+    <?php require "_head.php"; ?>
     <body>
         <div data-role="page" id="index">
             <div data-role="header" data-fixed="true">
