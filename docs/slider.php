d5b2cc9cb0c7fd9dfd5756d447efe08495a75628
c9s <cornelius.howl@gmail.com>
2013-01-10 00:45:35 +0800

    Add slider example page.

---
+++ docs/slider.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<?php require "_head.php"; ?>
+<body>
+    <div data-role="page" id="index">
+        <div data-role="header" data-fixed="true">
+            <h1>µMobi: Widget</h1>
+        </div>
+        <div data-role="content">
+            <h2>Range input</h2>
+            <input type="range" name="radio" value="2"/>
+        </div>
+        <div data-role="footer" data-fixed="true">
+            <div>Footer</div>
+        </div>
+    </div>
+</body>
+</html>
