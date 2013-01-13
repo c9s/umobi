cd83204cc339095c263b158adb219c883bdfe1ae
c9s <cornelius.howl@gmail.com>
2013-01-13 22:28:47 +0800

    swap article and section.

--- docs/index.php
+++ docs/index.php
@@ -2,24 +2,18 @@
 <html>
     <?php require "_head.php"; ?>
     <body>
-        <section data-role="page" id="index">
+        <article data-role="page" id="index">
             <header data-fixed="true">
                 <h1>µMobi</h1>
             </header>
-            <article data-role="content">
+            <section data-role="content">
                 <ul data-role="listview" data-inset="true">
-                    <li><a href="pages.php">Pages</a></li>
-                    <li><a href="buttons.php">Buttons</a></li>
-                    <li><a href="forms.php">Form Widgets</a></li>
-                    <li><a href="splitview.php">SplitView</a></li>
-                    <li><a href="content_formatting.php">Content Formatting</a></li>
-                    <li><a href="slider.php">Slider</a></li>
-                    <li><a href="minified.php">With Minified JS/CSS</a></li>
+                    <?php require "_index.php"; ?>
                 </ul>
-            </article>
+            </section>
             <footer data-fixed="true">
                 <div>Footer</div>
             </footer>
-        </section>
+        </article>
     </body>
 </html>
