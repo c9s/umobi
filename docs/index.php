0efff04f5b7531ccdebc9ce752aa33c5e39a1449
c9s <cornelius.howl@gmail.com>
2013-01-13 22:18:33 +0800

    Add slider

--- docs/index.php
+++ docs/index.php
@@ -13,7 +13,7 @@
                     <li><a href="forms.php">Form Widgets</a></li>
                     <li><a href="splitview.php">SplitView</a></li>
                     <li><a href="content_formatting.php">Content Formatting</a></li>
-
+                    <li><a href="slider.php">Slider</a></li>
                     <li><a href="minified.php">With Minified JS/CSS</a></li>
                 </ul>
             </div>
