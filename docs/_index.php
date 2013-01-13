0c76643fb0ac45f6db033d4c9fd706973ea4ae62
c9s <cornelius.howl@gmail.com>
2013-01-13 22:29:58 +0800

    Add _index.php

---
+++ docs/_index.php
@@ -0,0 +1,7 @@
+<li><a href="pages.php">Pages</a></li>
+<li><a href="buttons.php">Buttons</a></li>
+<li><a href="forms.php">Form Widgets</a></li>
+<li><a href="splitview.php">SplitView</a></li>
+<li><a href="content_formatting.php">Content Formatting</a></li>
+<li><a href="slider.php">Slider</a></li>
+<li><a href="minified.php">With Minified JS/CSS</a></li>
